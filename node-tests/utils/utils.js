05330e44f4ff199cf247ea3a2c33801d32e7703f
Romeo Enso <enso123456@gmail.com>
2018-05-25 09:51:32 +0800

    added expect test module

--- node-tests/utils/utils.js
+++ node-tests/utils/utils.js
@@ -1,3 +1,12 @@
 module.exports.add = (a, b) => a + b
 
 module.exports.square = (n) => n * n
+
+module.exports.setName = (user, fullName) => {
+	const names = fullName.split(" ")
+
+	user.firstName = names[0];
+	user.lastName = names[1];
+
+	return user
+}
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,11 +1,12 @@
+const chai = require('chai')
+const expect = chai.expect
 const util = require('./utils')
+
 
 it('should add both number', () => {
 	var res = util.add(3, 5)
 
-	if (res != 8) {
-		throw new Error("Expected 8, but got " + res)
-	}
+	expect(res).to.be.a('number')
 })
 
 it('should square a number', () => {
@@ -14,4 +15,18 @@ it('should square a number', () => {
 	if (res != 9) {
 		throw new Error("Expected 9, but got " + res)
 	}
+})
+
+it ('should verify display first and last name are set', () => {
+	const userObj = {
+		firstName: 'Romeo',
+		lastName: 'Enso',
+		age: 24
+	}
+	const res = util.setName(userObj, "Romeo Enso")
+
+	expect(res)
+		.to.be
+		.include({ firstName: 'Romeo', lastName: 'Enso'})
+		.a('object')
 })
