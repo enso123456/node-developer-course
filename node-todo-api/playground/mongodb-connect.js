b8b45c1cc016dc0e382c155e8529d3087dc4ea2e
Romeo Enso <enso123456@gmail.com>
2018-05-28 10:56:25 +0800

    mongo playground

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const validator = require('validator')
 
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/TodoApp", (err, db) => {
@@ -38,8 +38,28 @@ const User = mongoose.model('User', {
 		type: String,
 		required: true,
 		trim: true,
-		minlength: 1
-	}
+		minlength: 1,
+		unique: true,
+		validate: {
+			validator: validator.isEmail,
+			message: '{VALUE} is not valid email'
+		}
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 6
+	},
+	tokens: [{
+		access: {
+			type: String,
+			required: true
+		},
+		token: {
+			type: String,
+			required: true
+		}
+	}]
 })
 
 const newUser = new User({
